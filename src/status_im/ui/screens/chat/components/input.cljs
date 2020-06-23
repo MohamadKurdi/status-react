e71da0d7cbab508b6997345d729b923435866704
Gheorghe Pinzaru <feross95@gmail.com>
2020-06-23 11:12:43 +0300

    New input UI

--- src/status_im/ui/screens/chat/components/input.cljs
+++ src/status_im/ui/screens/chat/components/input.cljs
@@ -3,6 +3,9 @@
             [quo.hooks :refer [use-keyboard-dimension]]
             [quo.react-native :as rn]
             [quo.react :as react]
+            [quo.core :as quo]
+            [quo.design-system.colors :as colors]
+            [status-im.ui.components.icons.vector-icons :as icons]
             [quo.animated :as animated]))
 
 (def max-int (.-MAX_SAFE_INTEGER ^js js/Number))
@@ -16,6 +19,7 @@
                     bottom-safe-area]}
             (use-keyboard-dimension)
             y           (.-y ^js props)
+            children    (.-children ^js props)
             delta-y     (animated/sub y keyboard-end-position)
             translate-y (animated/sub keyboard-height
                                       (animated/clamp delta-y 0 max-int)
@@ -29,11 +33,38 @@
                                  :transform [{:translateY (animated/multiply -1
                                                                              (animated/clamp translate-y 0 max-int))}]}
                          }
-          [rn/view {:flex 1}
-           [rn/text-input {:placeholder      "Text here"
-                           :height           64
-                           :background-color :black
-                           :width            "100%"}]]]))))))
+          (into [rn/view {:flex 1}]
+                (react/get-children children))]))))))
+
+
+(defn text-input  []
+  [rn/view {:style {:height           52
+                    :border-top-width 1
+                    :border-top-color (:ui-01 @colors/theme)
+                    :background-color (:ui-background @colors/theme)
+                    :align-items      :center
+                    :flex-direction   :row}}
+   [animated/view {:flex-direction :row}
+    [icons/icon :main-icons/commands {:color :grey}]
+    [icons/icon :main-icons/photo {:color :grey}]]
+   [animated/view {:style {:background-color           (:ui-01 @colors/theme)
+                           :height                     34
+                           :flex                       1
+                           :border-top-left-radius     16
+                           :border-top-right-radius    16
+                           :border-bottom-right-radius 4
+                           :border-bottom-left-radius  16
+                           :flex-direction             :row}}
+    [rn/text-input {:flex               1
+                    :line-height        22
+                    :font-size          15
+                    :padding-vertical   6
+                    :padding-horizontal 12
+                    :placeholder        "Message"}]
+    [icons/icon :main-icons/sticker {:color :grey}]
+    [icons/icon :main-icons/commands {:color :grey}]
+    ]])
 
 (defn chat-input [y]
-  [accessory-view {:y y}])
+  [accessory-view {:y y}
+   [text-input]])
